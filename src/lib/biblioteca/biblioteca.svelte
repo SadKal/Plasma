1243a95b1ea090963112768f51fedac298aa6caf
Pablo San Segundo <pablosansegundo98@gmail.com>
2023-10-19 18:49:11 +0200

    Recent section finished-ish

--- src/lib/biblioteca/biblioteca.svelte
+++ src/lib/biblioteca/biblioteca.svelte
@@ -8,13 +8,13 @@
 	<div class="main_biblioteca__title">Tu biblioteca</div>
 	<div class="main_biblioteca__recientes">
 		<div class="main_biblioteca__recientes-title">Jugado recientemente</div>
-		<div class="main_biblioteca__recientes-thumbnails" >
-            <div class="main_biblioteca__recientes-thumbnails__large"></div>
-            <div class="main_biblioteca__recientes-thumbnails__small"></div>
-            <div class="main_biblioteca__recientes-thumbnails__small"></div>
-            <div class="main_biblioteca__recientes-thumbnails__small"></div>
-            <div class="main_biblioteca__recientes-thumbnails__small"></div>
-        </div>
+		<div class="main_biblioteca__recientes-thumbnails">
+			<div class="main_biblioteca__recientes-thumbnails__large" />
+			<div class="main_biblioteca__recientes-thumbnails__small" />
+			<div class="main_biblioteca__recientes-thumbnails__small" />
+			<div class="main_biblioteca__recientes-thumbnails__small" />
+			<div class="main_biblioteca__recientes-thumbnails__small" />
+		</div>
 	</div>
 </div>
 <div class="urgames_biblioteca">
@@ -30,10 +30,21 @@
 	$topbar-text-color: rgb(209, 51, 100);
 	$topbar-background-color: rgb(0, 74, 64);
 
-    $cyan-gradient: linear-gradient(30deg, rgba(0,255,224,1) 0%, rgba(2,0,36,1) 100%); 
+	$greenish-gradient: linear-gradient(
+		30deg,
+		rgb(0, 255, 170) 0%,
+		rgb(0, 0, 0) 100%
+	);
+    $purplish-gradient: linear-gradient(
+		30deg,
+		rgb(153, 0, 255) 0%,
+		rgb(0, 0, 0) 100%
+	);
 
 	.main_biblioteca {
 		background-color: unset;
+        border-bottom: 7px solid $topbar-text-color;
+        padding-bottom: 100px;
 
 		&__title {
 			padding: 10rem;
@@ -42,32 +53,42 @@
 			font-family: "Montserrat";
 			font-weight: 600;
 			color: $topbar-text-color;
+            letter-spacing: 3px;
 		}
 
 		&__recientes {
-            width: 80%;
-            margin: 0 auto;
+			width: 80%;
+			margin: 0 auto;
 
 			&-title {
 				font-family: "Montserrat";
 				font-weight: 600;
 				color: $topbar-text-color;
-				font-size: 2rem;
+				font-size: 2.5rem;
+				margin-bottom: 20px;
+				letter-spacing: 1px;
 			}
 
-            &-thumbnails{
+			&-thumbnails {
+				&__large {
+					background: $greenish-gradient;
+                    background-size: 150%;
+					border-radius: 2%;
+					height: 350px;
+					width: 400px;
+                    display: inline-block;
+				}
 
-                &__large {
-                    
-                    background: $cyan-gradient;
+				&__small {
+                    background: $purplish-gradient;
+                    background-size: 200%;
+                    border-radius: 3%;
                     height: 250px;
-                    
-                }
-
-                &__small {
-
-                }
-            }
+                    width: 175px;
+                    margin-left: 30px;
+                    display: inline-block;
+				}
+			}
 		}
 	}
 </style>

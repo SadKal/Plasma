585ffd61db91265cff351b960a69cce293343642
Pablo San Segundo <pablosansegundo98@gmail.com>
2023-10-20 14:15:36 +0200

    select is a pain in the ass

--- src/lib/biblioteca/tusjuegos/select.svelte
+++ src/lib/biblioteca/tusjuegos/select.svelte
@@ -1,43 +1,39 @@
 <script>
-	export let label = 'Ordenar por';
-	export let size = 3;
-	export let values = ["Más jugados", "Nombre", "Fecha de adquisición"];
-	export let selectedValue = 'Mas jugados';
+	export let label = "Ordenar por";
+	export let values = [
+		"Ordenar por",
+		"Más jugados",
+		"Nombre",
+		"Fecha de adquisición",
+	];
+	export let selectedValue = "Ordenar por";
 	export let disabled = false;
 	export let eventClick = (_) => {};
 
 	function selectValue(value) {
-		selectedValue =
-			value === selectedValue ? undefined : value;
+		selectedValue = value === selectedValue ? undefined : value;
 		eventClick(selectedValue);
 	}
 </script>
 
-<label>
-	<span>{label}</span>
 
 	<select
 		aria-label={label}
 		value={selectedValue}
 		{disabled}
 		on:click={() => selectValue()}
-		{...$$restProps}
 	>
 		{#each values as value (value)}
 			<option
 				{disabled}
-				on:click|stopPropagation={() =>
-					selectValue(value)}
+				on:click|stopPropagation={() => selectValue(value)}
 				{value}>{value}</option
 			>
 		{/each}
 	</select>
-</label>
+
 
 <style>
-	label {
-		margin-right: 20px;
-	}
 	select {
 		width: 120px;
 		overflow: hidden;

4fe2330cefae0025da43a08cc6b5c897dcb9da6f
Pablo San Segundo <pablosansegundo98@gmail.com>
2023-10-30 19:54:03 +0100

    sorting actually updates screen (1st approx.?)

--- src/lib/library/yourgames/content_insides/gamelist.svelte
+++ src/lib/library/yourgames/content_insides/gamelist.svelte
@@ -1,54 +1,39 @@
 <script>
 	import user from "../../../../data/testuser.json";
-    import Select from "./select.svelte"; 
+	import Select from "./select.svelte";
 
 	let games = user.gamesOwned;
 	sortBy("Nombre");
 
-
-
-	function compareDate(a, b) {
-
-	}
-
-
-	function sortBy(selectedValue){
+	function sortBy(selectedValue) {
 		if (selectedValue === "Nombre") {
-			games.sort((a , b) => {
-				if (a.name < b.name)
-					return -1;
-				if (a.name > b.name)
-					return 1;
-				if (a.name === b.name)
-					return 0;
-				
-			})
-
-		} 
+			games.sort((a, b) => {
+				if (a.name < b.name) return -1;
+				if (a.name > b.name) return 1;
+				if (a.name === b.name) return 0;
+			});
+		}
 		if (selectedValue === "Más jugados") {
 			games.sort((a, b) => {
 				return -(a.hrsPlayed - b.hrsPlayed);
-			})
-			
+			});
 		}
 		if (selectedValue === "Fecha adq.") {
-			//games.sort(compareDate)
-			//alert("fecha");
+			//Needs to be implemented: add dates to jsons and research date manipulation (uh oh)
 		}
-		console.log(">>>>>>", games)
+		games = games;
+		//console.log(">>>>>>", games)
 	}
-
-	
-
 </script>
 
 <span class="library--urgames__title">Tus juegos</span>
 
-<Select 
-label = "Ordenar por"
-values = {["Nombre", "Más jugados", "Fecha adq."]}
-selectedValue = "Nombre"
-eventClick={sortBy}/>
+<Select
+	label="Ordenar por"
+	values={["Nombre", "Más jugados", "Fecha adq."]}
+	selectedValue="Nombre"
+	eventClick={sortBy}
+/>
 
 {#each games as game}
 	<div
@@ -65,20 +50,19 @@ eventClick={sortBy}/>
 		width: 275px;
 		display: inline-block;
 		margin-right: 30px;
-        margin-bottom: 10px;
+		margin-bottom: 10px;
 		background-size: contain;
 		background-repeat: round;
-        margin-top: 30px;
-        border-radius: 8px;
+		margin-top: 30px;
+		border-radius: 8px;
 	}
 	.library--urgames__title {
-			position: absolute;
-			margin-top: -50px;
-			font-family: "Montserrat";
-			font-weight: 600;
-			color: var(--text-color);
-			font-size: 3rem;
-			letter-spacing: -1px;
-
+		position: absolute;
+		margin-top: -50px;
+		font-family: "Montserrat";
+		font-weight: 600;
+		color: var(--text-color);
+		font-size: 3rem;
+		letter-spacing: -1px;
 	}
 </style>

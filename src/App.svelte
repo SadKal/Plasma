08a75936c2e61577cc7c2f1cae0563292358faad
Pablo San Segundo <pablosansegundo98@gmail.com>
2023-11-13 14:04:34 +0100

    unnecessary file cleanup (oopsie)

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,41 @@
 <script>
-    import { onMount } from "svelte";
-    import Header from "$header/Header.svelte";
-    import Page from "$utils/Page.svelte";
-    import pagesStore from "$stores/pages";
-    import Footer from "./Footer/Footer.svelte";
-    import Login from "./lib/login/Login.svelte";
-    import Register from "./lib/login/Register.svelte";
+	import { onMount } from "svelte";
+	import Header from "$header/Header.svelte";
+	import Page from "$utils/Page.svelte";
+	import pagesStore from "$stores/pages";
+	import Footer from "./Footer/Footer.svelte";
+	import Login from "./lib/login/Login.svelte";
+	import Register from "./lib/login/Register.svelte";
 
-    $:{
-        if($pagesStore.active=='Inicio'){
-            scrollOnLoad();
-        }
-    }
-    
-    function scrollOnLoad(){
-        requestAnimationFrame(() => {
-            if(window.matchMedia("(max-width: 420px)").matches){
-                window.scrollTo(0,400);
-            }   
-            else{
-                window.scrollTo(0, 90);
-            };
-        });
-    }
-    onMount(()=> {
-        //Odio los navegadores modernos
-        //El navegador tiene en el historial una variable que hace que vuelva al inicio de la pagina al cargar
-        if ('scrollRestoration' in history) {
-            history.scrollRestoration = 'manual';
-        }
-    });
+	$: {
+		if ($pagesStore.active == "Inicio") {
+			scrollOnLoad();
+		}
+	}
+
+	function scrollOnLoad() {
+		requestAnimationFrame(() => {
+			if (window.matchMedia("(max-width: 420px)").matches) {
+				window.scrollTo(0, 400);
+			} else {
+				window.scrollTo(0, 90);
+			}
+		});
+	}
+	onMount(() => {
+		//Odio los navegadores modernos
+		//El navegador tiene en el historial una variable que hace que vuelva al inicio de la pagina al cargar
+		if ("scrollRestoration" in history) {
+			history.scrollRestoration = "manual";
+		}
+	});
 </script>
 
 <div class="main">
-    <!-- <Header/> 
-    <Page/>  -->
-        <!-- <Login/> -->
-        <Register/>
-    <!-- <Footer/>  -->
+	<Header />
+	<Page />
+	<Footer />
 </div>
-    
-    
+
 <style lang="scss">
 </style>
- 
